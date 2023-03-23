0a82aaecb4cc824fca4475b4d4889c14387d2beb
nawm77 <alpha050603@gmail.com>
2023-03-24 01:05:18 +0300

    select file for lab4

---
+++ src/main/resources/SQL/Select.sql
@@ -0,0 +1,13 @@
+/*Lab 4*/
+SELECT * FROM "Testdrive_list";
+SELECT * FROM "Available_cars";
+SELECT * FROM "Car_list";
+SELECT * FROM "Customer";
+SELECT * FROM "Employee";
+SELECT * FROM "Human";
+SELECT * FROM "Invoices";
+SELECT * FROM "Order";
+SELECT * FROM "Order_status";
+SELECT * FROM "Parts";
+SELECT * FROM "Position";
+SELECT * FROM "Testdrive_list";
